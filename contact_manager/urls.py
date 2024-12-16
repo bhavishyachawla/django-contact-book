3b7bf02a056d4dfe5dfa1f389b9c2f5560ebdc1f
Bhavishya <125631140+bhavishyachawla@users.noreply.github.com>
2024-12-16 11:24:24 +0530

    Add files via upload

---
+++ contact_manager/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for contact_manager project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from contacts import views  # Import views from the 'contacts' app
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    # Password Reset Views (Optional)
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('', views.contact_list, name='contact_list'),
+    path('create/', views.contact_create, name='contact_create'),
+    path('update/<int:pk>/', views.contact_update, name='contact_update'),
+    path('delete/<int:pk>/', views.contact_delete, name='contact_delete'),
+]
+
